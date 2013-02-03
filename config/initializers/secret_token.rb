093a1b894a186f85b6715eabd2ccb0e14cc19303
Ben Greenberg <bsgreenb@gmail.com>
2013-02-03 15:43:11 -0800

    Got rails started here

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thelifegame::Application.config.secret_token = '0f7720d85f7b7f55acbee9285db53c02a32c568f7456ef5dec2c6747f806c9e6f79ba9caae8bda55a35bea1aca8d1dc39141e2577520c96ccc6439c6c277af05'
